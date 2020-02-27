4730a02d5dc2bc5e3b0ce51c10f4a4e9453df5d7
Vadim Mishkhozhev <vadim.stug@gmail.com>
2020-02-27 23:29:32 +0700

    Добавлена верхняя панель шапки

--- footer.php
+++ footer.php
@@ -11,24 +11,7 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gbomotors' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'gbomotors' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gbomotors' ), 'gbomotors', '<a href="http://underscores.me/">Vadim Mishkhozhev</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+	</div><!-- site-wrapper -->
 
 <?php wp_footer(); ?>
 
